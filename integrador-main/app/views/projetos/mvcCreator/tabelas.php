e5110e7e8787b116f538a6988cde2ccb47cefbc2
CelsoGasparin <134729228+CelsoGasparin@users.noreply.github.com>
2026-09-04 17:01:35 -0300

    arquivos duplicados excluidos² - celso

--- integrador-main/app/views/projetos/mvcCreator/tabelas.php
+++
@@ -1,8 +0,0 @@
-<div class="mvc-etapa">
-    <p class="mvc-subtitulo">Selecione as tabelas que farão parte do sistema MVC:</p>
-    <div id="container-tabelas"><p style="color: #666;">Carregando tabelas do banco...</p></div>
-    <div class="mvc-acoes">
-        <a href="?step=opcoes" class="mvc-etapa-botao">Próximo: Opções de Geração →</a>
-        <a href="?step=configurar" class="mvc-etapa-botao mvc-etapa-botao-secundario">← Voltar</a>
-    </div>
-</div>
